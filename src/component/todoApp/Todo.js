21af6ce0a9a37a1fd8f9ee9c7525f3e5cee18867
editaahn <41330862+editaahn@users.noreply.github.com>
2020-09-28 07:02:49 +0900

    (re-commit) todo를 원하는 위치에 넣게끔 수정
    1.소팅 구조 변경
    A) manager 객체에 들어있던 순서(order) 배열을 신규 객체인 orders로 관리 위치 변경
    B) manager를 렌더할 때, orders 객체 내 기준으로 소팅하게끔 변경
    C) 추가/삭제/이동 시 orders를 같이 조정
    D) orders에 변화가 일어나면 re-render
    
    2.드래그 이벤트 고도화
    A) onDrag 시, id string parsing이 아닌 attribute값으로 이동할 manager_id / todo_id 확인
    B) redux에서 관리중인 순서를 reduce 메서드로 교체 (배열의 정렬 index를 변경)
    
    3.드래그 시작 후 ~ 종료 전 이벤트 추가
    A) onDragEnter / onDragLeave 이벤트 추가
    B) 위 이벤트 발생 시, li의 class를 변경하여, 상단에 ::before 가상요소가 들어가도록 함

--- src/component/todoApp/Todo.js
+++ src/component/todoApp/Todo.js
@@ -3,12 +3,12 @@ import React from "react";
 const Todo = ({
   drag,
   content,
-  // id: dom 접근용 element id, todo_id: todo의 DB 고유식별자
-  id,
+  manager_id,
   todo_id,
   deleteTodo,
   setTodoEditMode,
   setEditingTodoID,
+  index,
 }) => {
   const confirmDeletion = () => {
     window.confirm("정말 삭제하시겠습니까?") && deleteTodo(todo_id);
@@ -18,13 +18,22 @@ const Todo = ({
     setEditingTodoID(todo_id);
   };
   return (
-    <li id={id} className="todo" draggable={true} onDragStart={(e) => drag(e)}>
-      <p className="todo__content" onClick={editEvt}>
-        {content}
-      </p>
-      <button className="button--delete" onClick={confirmDeletion}>
-        삭제
-      </button>
+    <li
+      className="todo-wrapper"
+      draggable={true}
+      onDragStart={(e) => drag(e)}
+      manager_id={manager_id}
+      todo_id={todo_id}
+      index={index}
+    >
+      <div className="todo">
+        <p className="todo__content" onClick={editEvt}>
+          {content}
+        </p>
+        <button className="button--delete" onClick={confirmDeletion}>
+          삭제
+        </button>
+      </div>
     </li>
   );
 };
--- src/redux-module/todoList.js
+++ src/redux-module/todoList.js
@@ -48,170 +48,197 @@ const initialState = {
     EDIT_TODO: false,
     MOVE_TODO: false,
   },
-  history: []
+  history: [],
+  orders: {},
 };
-
 
 //리듀서
 const todoList = handleActions(
   {
-    [GET_TODO_DB] : state => ({
+    [GET_TODO_DB]: (state) => ({
       ...state,
       loading: {
         ...state.loading,
-        GET_TODO_DB: true
-      }
+        GET_TODO_DB: true,
+      },
     }),
-    [GET_TODO_DB_SUCCESS] : (state, {payload: data}) => ({
+    [GET_TODO_DB_SUCCESS]: (state, { payload: data }) => ({
       ...state,
       loading: {
         ...state.loading,
-        GET_TODO_DB: false
+        GET_TODO_DB: false,
       },
       managers: data.managers,
       todos: data.todos,
-      history: data.history
+      history: data.history,
+      orders: data.todos.reduce((acc, todo) => {
+        todo.manager_id in acc
+          ? acc[todo.manager_id].push(todo.todo_id)
+          : acc[todo.manager_id] = [todo.todo_id];
+        return acc;
+      }, {}),
     }),
-    [GET_TODO_DB_FAILURE] : state => ({
+    [GET_TODO_DB_FAILURE]: (state) => ({
       ...state,
       loading: {
         ...state.loading,
-        GET_TODO_DB: false
-      }
-    }),
-    [SET_MANAGER_NAME] : state => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        SET_MANAGER_NAME: true
-      }
-    }),
-    [SET_MANAGER_NAME_SUCCESS] : (state, {payload: newManager}) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        SET_MANAGER_NAME: false
+        GET_TODO_DB: false,
       },
-      managers: 
-        state.managers.reduce((acc,prevManager) => {
-          prevManager.manager_id === newManager.manager_id
-            ? acc.push(newManager)
-            : acc.push(prevManager)
-          return acc
-        }, [])
     }),
-    [SET_MANAGER_NAME_FAILURE] : state => ({
+    [SET_MANAGER_NAME]: (state) => ({
       ...state,
       loading: {
         ...state.loading,
-        SET_MANAGER_NAME: false
-      }
-    }),
-    [ADD_TODO] : state => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        ADD_TODO: true
-      }
-    }),
-    [ADD_TODO_SUCCESS] : (state, {payload: result}) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        ADD_TODO: false
+        SET_MANAGER_NAME: true,
       },
-      todos: state.todos.concat(result.todo),
-      history: state.history.concat({...result.history,...result.todo}),
     }),
-    [ADD_TODO_FAILURE] : state => ({
+    [SET_MANAGER_NAME_SUCCESS]: (state, { payload: newManager }) => ({
       ...state,
       loading: {
         ...state.loading,
-        ADD_TODO: false
-      }
-    }),
-    [DELETE_TODO] : state => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        ADD_TODO: true
-      }
-    }),
-    [DELETE_TODO_SUCCESS] : (state, {payload: result}) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        DELETE_TODO: false
+        SET_MANAGER_NAME: false,
       },
-      todos: state.todos.filter(todo => todo.todo_id !== result.todo.todo_id),
-      history: state.history.concat({...result.history,...result.todo}),
+      managers: state.managers.reduce((acc, prevManager) => {
+        prevManager.manager_id === newManager.manager_id
+          ? acc.push(newManager)
+          : acc.push(prevManager);
+        return acc;
+      }, []),
     }),
-    [DELETE_TODO_FAILURE] : state => ({
+    [SET_MANAGER_NAME_FAILURE]: (state) => ({
       ...state,
       loading: {
         ...state.loading,
-        DELETE_TODO: false
-      }
-    }),
-    [MOVE_TODO] : state => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        MOVE_TODO: true
-      }
-    }),
-    [MOVE_TODO_SUCCESS] : (state, {payload: result}) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        MOVE_TODO: false
+        SET_MANAGER_NAME: false,
       },
-      todos: 
-        state.todos.reduce((acc, prevTodo) => {
-          prevTodo.todo_id === result.todo.todo_id
-            ? acc.push(result.todo)
-            : acc.push(prevTodo)
-          return acc
-        }, []),
-      history: state.history.concat({...result.history,...result.todo}),
     }),
-    [MOVE_TODO_FAILURE] : state => ({
+    [ADD_TODO]: (state) => ({
       ...state,
       loading: {
         ...state.loading,
-        MOVE_TODO: false
-      }
-    }),
-    [EDIT_TODO] : state => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        EDIT_TODO: true
-      }
-    }),
-    [EDIT_TODO_SUCCESS] : (state, {payload: result}) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        EDIT_TODO: false
+        ADD_TODO: true,
       },
-      todos: 
-        state.todos.reduce((acc, prevTodo) => {
-          prevTodo.todo_id === result.todo.todo_id
-            ? acc.push(result.todo)
-            : acc.push(prevTodo)
-          return acc
-        }, []),
-      history: state.history.concat({...result.history,...result.todo}),
     }),
-    [EDIT_TODO_FAILURE] : state => ({
+    [ADD_TODO_SUCCESS]: (state, { payload: { todo, history } }) => ({
       ...state,
       loading: {
         ...state.loading,
-        EDIT_TODO: false
+        ADD_TODO: false,
+      },
+      todos: state.todos.concat(todo),
+      history: state.history.concat({ ...history, ...todo }),
+      orders: {
+        ...state.orders,
+        [todo.manager_id]: [todo.todo_id, ...state.orders[todo.manager_id]]
       }
     }),
-  }, initialState
-)
+    [ADD_TODO_FAILURE]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        ADD_TODO: false,
+      },
+    }),
+    [DELETE_TODO]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        ADD_TODO: true,
+      },
+    }),
+    [DELETE_TODO_SUCCESS]: (state, { payload: { todo, history } }) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        DELETE_TODO: false,
+      },
+      todos: state.todos.filter(prevTodo => prevTodo.todo_id !== todo.todo_id),
+      history: state.history.concat({ ...history, ...todo }),
+      orders: {
+        ...state.orders,
+        [history.prev_manager_id]: state.orders[history.prev_manager_id].filter(prevTodo => prevTodo !== todo.todo_id)
+      }
+    }),
+    [DELETE_TODO_FAILURE]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        DELETE_TODO: false,
+      },
+    }),
+    [MOVE_TODO]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        MOVE_TODO: true,
+      },
+    }),
+    [MOVE_TODO_SUCCESS]: (state, { payload: { todo, history, index } }) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        MOVE_TODO: false,
+      },
+      todos: state.todos.reduce((acc, prevTodo) => {
+        return prevTodo.todo_id === todo.todo_id
+          ? acc.concat(todo)
+          : acc.concat(prevTodo);
+      }, []),
+      history: state.history.concat({ ...history, ...todo }),
+      orders: {
+        ...state.orders,
+        [history.prev_manager_id]: 
+          state.orders[history.prev_manager_id].filter( movedTodoID => movedTodoID !== todo.todo_id ),
+        [todo.manager_id]: 
+          !state.orders[todo.manager_id] 
+            ? [todo.todo_id] 
+            : state.orders[todo.manager_id].reduce((acc, todoInOrder, i, arr) => {
+              if (index === arr.length && i === arr.length-1)
+                return acc.concat(todoInOrder, todo.todo_id) // 빈 공간에 drop한 경우 끝으로 붙임
+              else if (i === index) 
+                return acc.concat(todo.todo_id, todoInOrder) // todo li들 사이에 drop한 경우 drop 위치에 끼움
+              else 
+                return acc.concat(todoInOrder)
+            }, []),
+      }
+    }),
+    [MOVE_TODO_FAILURE]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        MOVE_TODO: false,
+      },
+    }),
+    [EDIT_TODO]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        EDIT_TODO: true,
+      },
+    }),
+    [EDIT_TODO_SUCCESS]: (state, { payload: result }) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        EDIT_TODO: false,
+      },
+      todos: state.todos.reduce((acc, prevTodo) => {
+        prevTodo.todo_id === result.todo.todo_id
+          ? acc.push(result.todo)
+          : acc.push(prevTodo);
+        return acc;
+      }, []),
+      history: state.history.concat({ ...result.history, ...result.todo }),
+    }),
+    [EDIT_TODO_FAILURE]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        EDIT_TODO: false,
+      },
+    }),
+  },
+  initialState
+);
 
 export default todoList;
