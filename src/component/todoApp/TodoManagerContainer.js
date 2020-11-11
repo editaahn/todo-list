e5b1cfec701448f1021137392185251aca60e402
editaahn <41330862+editaahn@users.noreply.github.com>
2020-11-11 16:11:45 +0900

    add responsive size configurations

--- src/component/todoApp/TodoManagerContainer.js
+++ src/component/todoApp/TodoManagerContainer.js
@@ -3,38 +3,45 @@ import { connect } from "react-redux";
 import * as actions from "../../redux-module/todoList";
 import TodoManager from "./TodoManager";
 
-const TodoManagerContainer = ({ managers, todos, getTodoDB, deleteManager, setManagerName, addTodo, deleteTodo, editTodo, moveTodo, loading, orders }) => {
+const TodoManagerContainer = ({ 
+  managers, todos, getTodoDB, deleteManager, setManagerName, addTodo, deleteTodo, editTodo, moveTodo, loading, orders 
+}) => {
   useEffect(() => {
     getTodoDB();
   }, [getTodoDB]);
 
   return (
-    <main className="app">
-      {managers &&
-        // !loading.GET_TODO_DB &&
-        managers.map((manager) => (
-          <TodoManager
-            key={manager.manager_id}
-            id={manager.manager_id}
-            name={manager.name}
-            todos={todos.filter(todo => 
-              todo.manager_id === manager.manager_id)}
-            deleteManager={id => 
-              deleteManager({ 'manager_id' : id })}
-            setManagerName={(id,name) => 
-              setManagerName({'manager_id': id, "name": name})}
-            addTodo={(id,content,order) => 
-              addTodo({'manager_id': id, 'content': content, 'order': order.join(',')})}
-            deleteTodo={(manager_id,id,order) => 
-              deleteTodo({'manager_id': manager_id, 'todo_id': id, 'order': order.filter(todo => todo !== id).join(',')})}
-            editTodo={(id,content) => 
-              editTodo({'todo_id': id, 'content': content})}
-            moveTodo={data =>
-              moveTodo(data)}
-            order={orders[manager.manager_id] || []}
-          />
-        ))}
-    </main>
+    <div 
+      className="app-wrapper" 
+      style={{ 'overflowX': managers.length > 3 }}
+    >
+      <main className="app" >
+        {managers &&
+          // !loading.GET_TODO_DB &&
+          managers.map((manager) => (
+            <TodoManager
+              key={manager.manager_id}
+              id={manager.manager_id}
+              name={manager.name}
+              todos={todos.filter(todo => 
+                todo.manager_id === manager.manager_id)}
+              deleteManager={id => 
+                deleteManager({ 'manager_id' : id })}
+              setManagerName={(id,name) => 
+                setManagerName({'manager_id': id, "name": name})}
+              addTodo={(id,content,order) => 
+                addTodo({'manager_id': id, 'content': content, 'order': order.join(',')})}
+              deleteTodo={(manager_id,id,order) => 
+                deleteTodo({'manager_id': manager_id, 'todo_id': id, 'order': order.filter(todo => todo !== id).join(',')})}
+              editTodo={(id,content) => 
+                editTodo({'todo_id': id, 'content': content})}
+              moveTodo={data =>
+                moveTodo(data)}
+              order={orders[manager.manager_id] || []}
+            />
+          ))}
+      </main>
+  </div>
   );
 }
 
