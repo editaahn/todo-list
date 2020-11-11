872737246cb9fa5d6e4867bbddd0147b23071fb3
editaahn <41330862+editaahn@users.noreply.github.com>
2020-11-11 12:21:22 +0900

    fix css of history center

--- src/component/historyWing/HistoryContainer.js
+++ src/component/historyWing/HistoryContainer.js
@@ -9,8 +9,15 @@ const HistoryContainer = ({isHistoryOpen}) => {
     return a.date < b.date ? 1 : -1;
   });
 
+  // const width = window.width
+
   return (
-    <aside className="history-center" style={{ 'right' : isHistoryOpen ? '0' : '-28rem' }}>
+    <aside 
+      className="history-center" 
+      style={{ 
+        'right': isHistoryOpen ? '0' : `-30rem`,
+      }}
+    >
       <header className="history-center__header">
         <h2>History</h2>
         <p>최근 12시간 이력을 보여줍니다.</p>
